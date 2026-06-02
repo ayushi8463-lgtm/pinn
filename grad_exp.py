6913f221187d98ccf16d39adb9b588a876e8465a
Ayushi Gupta <ayushi8463@gmail.com>
2026-06-02 18:19:41 +0530

    higher order deriv

--- grad_exp.py
+++ grad_exp.py
@@ -26,42 +26,74 @@ class var:
     def __radd__(self, other):
         return self.__add__(other)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other):#supports subtraction even if other operand is not var object
         return fnsub(var(other), self)
-    def __rmul__(self, other):
+    def __rmul__(self, other):#supports multiplication even if other operand is not var object
         return fnmul(var(other), self)
+    def __truediv__(self, other):
+        if not isinstance(other, var):
+            other = var(other)
+        return fndiv(self, other)
+
+    def __rtruediv__(self, other):
+        return fndiv(var(other), self)
 
 def fnsum(var1, var2):
-    x = lambda g: g
-    return var(var1.val + var2.val, [var1, var2], [x, x])
+    def grad1(g):
 
-def fnmul(var1, var2):
-    val1 = var1.val
-    val2 = var2.val
-    x1 = lambda g: var(g.val * val2) 
-    x2 = lambda g: var(g.val * val1)
-    return var(var1.val * var2.val, [var1, var2], [x1, x2])
-def fnmatmul(var1, var2):
-    val1 = var1.val
-    val2 = var2.val
-    x1 = lambda g: var(g.val @ val2.T)
-    x2 = lambda g: var(val1.T @ g.val)
-    return var(val1 @ val2, [var1, var2], [x1, x2])
-def fnsigmoid(v):
-    sig = 1 / (1 + np.exp(-v.val))
-    sig_var = var(sig)
-    x = lambda g: g * sig_var * (var(1) - sig_var)
-    return var(sig, [v], [x])
+        #this if statement is to fix an error encountered when adding bias velocity and momentum
+        if g.val.shape != var1.val.shape:
+            axis = (1,)#(n,batchsize) and (n,1) needed to be matched
+            #sum the values to get batch size*g. 
+            #This node doesnt have parents but we dont need it because we never differentiate twice wrt bw and bv
+            return var(g.val.sum(axis=axis, keepdims=True))  
+        return g
+    def grad2(g):
+        if g.val.shape != var2.val.shape:
+            axis = (1,)
+            return var(g.val.sum(axis=axis, keepdims=True))
+        return g
+    return var(var1.val + var2.val, [var1, var2], [grad1, grad2])
 
 def fnsub(var1, var2):
     x1= lambda g: g 
     x2= lambda g: g * var(-1)
     return var(var1.val - var2.val,[var1, var2], [x1, x2])
 
-def fnpow(v, power):
-    val_og = v.val
-    x = lambda g: g*var(power* (val_og **(power - 1)))
-    return var(v.val**power, [v], [x])
+def fnmul(var1, var2):
+    x1 = lambda g: g*var2
+    x2 = lambda g: g*var1
+    return var(var1.val * var2.val, [var1, var2], [x1, x2])
+def fnmatmul(var1, var2):
+    x1 = lambda g: g @ var(var2.val.T)
+    x2 = lambda g: var(var1.val.T) @ g
+    return var(var1.val @ var2.val, [var1, var2], [x1, x2])
+def fnexp(v):
+    exp_val = np.exp(v.val)
+    out=var(exp_val,[v],None)
+    x = lambda g:g*out
+    out.chainrules=[x]
+    return out
+
+def fndiv(var1, var2):
+    x1 = lambda g: g /var2
+    x2 = lambda g: g *var(-1)*var1/var2**2
+    return var(var1.val / var2.val, [var1, var2], [x1, x2])
+def fnsigmoid(v):
+    sig=var(np.ones_like(v.val))/(var(np.ones_like(v.val))+fnexp(v*var(-1)))#1/(1+e^-v)
+    return sig
+
+def fnsum_all(v):
+    x = lambda g: var(np.ones_like(v.val) * g.val)
+    return var(np.array([[v.val.sum()]]), [v], [x])#summing the gradients for all points in a batch
+
+def fnpow(var1, power):
+    x = lambda g: g*var(power)* (var1 **(power - 1))
+    return var(var1.val**power, [var1], [x])
+
+def fntanh(v):
+    return (fnexp(v) - fnexp(v * var(-1))) / (fnexp(v) + fnexp(v * var(-1)))
+
 
 
 def autograd(t):
@@ -86,7 +118,7 @@ def autograd(t):
         for parent, chainrule in zip(node.parents, node.chainrules):
             contrib=chainrule(g) 
             if parent in grads:
-                grads[parent] = var(grads[parent].val + contrib.val)
+                grads[parent] =grads[parent]+ contrib
             else:
                 grads[parent] = contrib
     return grads
@@ -100,3 +132,11 @@ def autograd(t):
 # dv=autograd(f)
 # dvda=dv[a]
 # print(f.val,dvda.val)
+
+# #supports higher order differentiation
+# a = var(np.array(3.0))
+# f = a ** 3
+# dfda = autograd(f)[a]#3a^2 = 27
+# d2fda2 = autograd(dfda)[a]#6a  = 18
+# d3fda3 = autograd(d2fda2)[a]#6  = 6
+# print(dfda.val, d2fda2.val, d3fda3.val) 
